b0d3803f4dc414ad416ec49a96586d10be0a3404
Freek <freekdh@gmail.com>
2022-02-04 21:49:09 +0100

    add a file parser

---
+++ file_parser.py
@@ -0,0 +1,24 @@
+from itertools import tee
+from dataclasses import dataclass
+from typing import Iterable
+
+@dataclass(frozen=True, eq=False)
+class Client:
+    liked_ingredients: set
+    disliked_ingredients: set
+
+class FileParser:
+    def __init__(self):
+        pass
+
+    def parse(self, file_path: str) -> Iterable[Client]:
+        with open(file_path) as file:
+            n_customers = int(next(file).strip())
+            for customer_id, (liked_ingredients_line, disliked_ingredients_line) in enumerate(zip(file, file)):
+                liked_ingredients_line =  liked_ingredients_line.strip().split(" ")
+                disliked_ingredients_line = disliked_ingredients_line.strip().split(" ")
+                
+                liked_ingredients = set(liked_ingredients_line[1:])
+                disliked_ingredients = set(disliked_ingredients_line[1:])
+
+                yield Client(liked_ingredients=liked_ingredients, disliked_ingredients=disliked_ingredients)
---
+++ tests/test_file_parser.py
@@ -0,0 +1,25 @@
+from pydoc import cli
+from pytest import fixture
+from ..file_parser import FileParser
+
+@fixture
+def example_input_file():
+    return "data/a_an_example.in.txt"
+
+@fixture
+def file_parser():
+    return FileParser()
+
+def test_parse(file_parser, example_input_file):
+    clients = set(file_parser.parse(example_input_file))
+    assert len(clients) == 3
+
+    liked = {"basil"}
+    disliked = {"pineapple"}
+
+    for client in clients:
+        if client.liked_ingredients == liked and client.disliked_ingredients == disliked:
+            found = True
+            break
+    
+    assert found
