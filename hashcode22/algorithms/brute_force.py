12cc156d79e6152041a902c3c6b5ac8637edf806
Freek <freekdh@gmail.com>
2022-02-09 14:39:58 +0100

    clean up the brute_force solver

--- hashcode22/algorithms/brute_force.py
+++ hashcode22/algorithms/brute_force.py
@@ -8,24 +8,30 @@ from hashcode22.solution import Solution
 
 
 class BruteForceAlgorithm:
-    def __init__(self):
-        pass
+    """The brute force algorithm simply explores all possible
+    ingredient combinations and chooses the combintation that
+    leads to the most clients eating the pizza.
+    """
 
     def solve(self, problem: Problem):
         ingredients = problem.get_ingredients()
         best_score, best_pizza = 0, None
         for consider_ingredients in self._powerset(ingredients):
-            pizza = self._make_pizza(consider_ingredients)
+            pizza = Pizza(ingredients=set(consider_ingredients))
             score = problem.get_score(pizza)
             if score > best_score:
                 best_score = score
                 best_pizza = pizza
         return Solution(pizza=best_pizza)
 
-    def _make_pizza(self, ingredients: Iterable[set]):
-        return Pizza(ingredients=set(ingredients))
+    def _powerset(self, iterable: Iterable) -> Iterable:
+        """Return the powerset of an iterable
 
-    def _powerset(self, iterable):
-        "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
+        Args:
+            iterable ([Iterable]): Any iterable
+
+        Returns:
+            [Iterable]: An iterable over the powerset of the input iterable
+        """
         s = list(iterable)
         return chain.from_iterable(combinations(s, r) for r in range(len(s) + 1))
