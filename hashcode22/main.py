5442e9fd1cf10108a1255ad3e9bb08c4d3851294
Ignacio Rozada <ignacio.rozada@1qbit.com>
2022-02-21 18:18:42 -0800

    added max_clique algorithm that finds a small clique and then builds a greedy solution on top of it

--- hashcode22/main.py
+++ hashcode22/main.py
@@ -1,35 +1,42 @@
 from file_parser import FileParser
 from problem import Problem
+from time import time
 
 from hashcode22.algorithms.brute_force import BruteForceAlgorithm
 from hashcode22.algorithms.genetic_algorithm import GeneticAlgorithm
 from hashcode22.algorithms.max_clique import MaxClique
+from hashcode22.algorithms.greedy_max_clique import GreedyMaxClique
 from hashcode22.output_writer import OutputWriter
 
-problems = [
-    "a_an_example.in.txt",
-    "b_basic.in.txt",
-    "c_coarse.in.txt",
-    "d_difficult.in.txt",
-    "e_elaborate.in.txt",
-]
+problems = {
+    'a':"a_an_example.in.txt",
+    'b':"b_basic.in.txt",
+    'c':"c_coarse.in.txt",
+    'd':"d_difficult.in.txt",
+    'e':"e_elaborate.in.txt",
+}
 
-algorithms = [
-    BruteForceAlgorithm(),
-    GeneticAlgorithm(),
-    MaxClique(),
-    MaxClique(heuristic=True),
-]
+algorithms = {
+    # 'brute_force': BruteForceAlgorithm(),
+    'genetic': GeneticAlgorithm(),
+    # 'max_clique': MaxClique()
+    'greedy_max_clique': GreedyMaxClique()
+    }
 
 if __name__ == "__main__":
-    index = 2
-    file_parser = FileParser()
-    clients = file_parser.parse(f"data/{problems[index]}")
-    problem = Problem(clients=clients)
+    t_init = time()
+    for problem_ix in ['d', 'e']:
+        for i in range(2):
+            print(f"\nSolving problem {problem_ix}")
+            file_parser = FileParser()
+            file_path = f"data/{problems[problem_ix]}"
+            clients = file_parser.parse(file_path)
+            problem = Problem(clients=clients)
 
-    for algorithm in algorithms:
-        solution = algorithm.solve(problem=problem)
+            for algorithm in algorithms:
+                solution = algorithms[algorithm].solve(problem=problem)
 
-        print(
-            f"algorithm {algorithm} f: = {algorithm.objective_function_pizza(pizza=solution.pizza)}"
-        )
+                print(
+                    f"algorithm {algorithm} f: = {algorithms[algorithm].objective_function_pizza(pizza=solution.pizza)}"
+                )
+    print(f"total time = {time() - t_init}")
